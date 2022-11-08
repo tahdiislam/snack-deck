dc798a202a38382d813bd72c3e87d59a83f1f334
tahdi-islam <tshdi60@gmail.com>
2022-11-08 23:50:14 +0600

    add service details

--- src/Pages/AllServices/ServiceDetails.jsx
+++ src/Pages/AllServices/ServiceDetails.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { FaStar, FaStarHalfAlt } from 'react-icons/fa';
 import { useLoaderData } from 'react-router-dom';
 
 const ServiceDetails = () => {
     const food = useLoaderData()
     console.log(food);
-    const {name, _id, image, ratings, price, description} = food.service;
+    const { name, _id, image, ratings, price, description } = food.service;
     return (
-        <div>
-            
+        <div className='mx-3 md:mx-auto'>
+            <div className='rounded-lg shadow-lg w-full md:w-2/3 bg-red-50 mx-auto p-4 my-4 '>
+                <img className='w-full rounded-md' src={image} alt="food image" />
+                <h2 className='text-5xl font-semibold my-4 ml-3 text-amber-900'>{name}</h2>
+                <div>
+                    <h3 className='text-2xl fond-semibold my-4'>Price: <span className='text-amber-900 text-4xl fond-semibold'>${price}</span></h3>
+                    <h3 className='text-2xl fond-semibold my-4 flex items-center'><span className='mr-2'>Ratings:</span> <span className='text-amber-900 text-4xl fond-semibold flex items-center'> <span className='mr-2 text-amber-400'>{ratings < 5 ? <FaStarHalfAlt /> : <FaStar />}</span> <span>{ratings}</span></span></h3>
+                </div>
+                <p className='text-lg '>
+                    {description}
+                </p>
+            </div>
         </div>
     );
 };
