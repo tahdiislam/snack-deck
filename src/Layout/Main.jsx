61a780dd967de16c2a42c7da1365d304e6d03a48
tahdi-islam <tshdi60@gmail.com>
2022-11-09 02:28:55 +0600

    add footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Shared/Footer';
 import Header from '../Shared/Header';
 
 const Main = () => {
@@ -7,6 +8,7 @@ const Main = () => {
         <div>
           <Header/>
           <Outlet/>
+          <Footer/>
         </div>
     );
 };
