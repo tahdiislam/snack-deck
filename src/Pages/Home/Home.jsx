14e7d4504650482c9dd55f76541fc1b4db51a259
tahdi-islam <tshdi60@gmail.com>
2022-11-08 11:24:57 +0600

    add image slider

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from './Component/Banner';
 
 const Home = () => {
     return (
         <div>
-          Hello Home  
+          <Banner/>            
         </div>
     );
 };
