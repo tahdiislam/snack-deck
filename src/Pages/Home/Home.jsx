caea3f8fa60660db413dbd3ebe36877d37a23d32
tahdi-islam <tshdi60@gmail.com>
2022-11-10 08:19:30 +0600

    add offers

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import AboutMe from './Component/AboutMe';
 import Banner from './Component/Banner';
 import Services from './Component/Services';
@@ -11,6 +12,21 @@ const Home = () => {
           <Services/>
           <AboutMe/>
           <SomeReviews/>
+          <div className="divider text-4xl font-semibold text-amber-900 mb-8">Offers</div>
+        <div className="p-6 py-12 bg-red-400 text-gray-50">
+          <div className="container mx-auto">
+            <div className="flex flex-col lg:flex-row items-center justify-between">
+              <h2 className="text-center text-6xl tracking-tighter font-bold">Up to
+                <br className="sm:hidden"/>20% Off
+              </h2>
+              <div className="space-x-2 text-center py-2 lg:py-0">
+                <span>Plus free shipping! Use code:</span>
+                <span className="font-bold text-lg">SNACKDECK20%</span>
+              </div>
+              <Link to="/services" rel="noreferrer noopener"><button className="btn btn-success">Order Now</button></Link>
+            </div>
+          </div>
+        </div>
         </div>
     );
 };
