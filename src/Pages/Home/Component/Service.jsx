db822fd9f3a5e705d04910781040d0685e22db11
tahdi-islam <tshdi60@gmail.com>
2022-11-10 16:37:17 +0600

    add react photo view

--- src/Pages/Home/Component/Service.jsx
+++ src/Pages/Home/Component/Service.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaStar, FaStarHalfAlt } from "react-icons/fa";
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 
 const Service = ({ service }) => {
     const { name, price, image, ratings, description, _id } = service;
     return (
         <div className="card w-96 glass">
             <figure>
-                {/* <img src={image} alt="car!" /> */}
-                    <img src={image} alt="" />
+                <PhotoProvider speed={() => 800}
+                    easing={(type) => (type === 2 ? 'cubic-bezier(0.36, 0, 0.66, -0.56)' : 'cubic-bezier(0.34, 1.56, 0.64, 1)')} >
+                    <PhotoView src={image}>
+                        <img src={image} style={{ objectFit: 'cover' }} alt="" />
+                    </PhotoView>
+                </PhotoProvider>
             </figure>
             <div className="card-body">
                 <h2 className="card-title text-amber-900">{name}</h2>
