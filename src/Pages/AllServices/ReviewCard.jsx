cc5ffae5d1cda2099337702a5deec9e7ef420d0e
tahdi-islam <tshdi60@gmail.com>
2022-11-10 08:05:42 +0600

    change the review card logo

--- src/Pages/AllServices/ReviewCard.jsx
+++ src/Pages/AllServices/ReviewCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import reviewLogo from '/review-icon.png';
 
 const ReviewCard = ({ review }) => {
     const { userName, email, reviewerPhoto, date, reviewText } = review;
@@ -7,14 +8,8 @@ const ReviewCard = ({ review }) => {
             <div className="flex flex-col max-w-sm mx-4 my-6 shadow-lg">
                 <div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 bg-gray-50">
                     <p className="relative px-6 py-1 text-lg italic text-center text-gray-800">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-8 h-8 text-fuchsia-600">
-                            <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
-                            <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
-                        </svg>{reviewText}
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="absolute right-0 w-8 h-8 text-fuchsia-600">
-                            <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
-                            <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
-                        </svg>
+                        <img className="w-6 h-6 text-fuchsia-600" src={reviewLogo} alt="" />{reviewText}
+                        <img className="absolute right-0 w-6 h-6 text-fuchsia-600" src={reviewLogo} alt="" />
                     </p>
                 </div>
                 <div className="flex flex-col items-center justify-center p-8 rounded-b-lg bg-amber-800 text-gray-50">
