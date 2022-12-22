1b4b6626b99917cce6d81f16ce0a0317ba1268c8
tahdi-islam <tshdi60@gmail.com>
2022-12-22 08:12:08 +0600

    add loader in home page

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,16 +1,40 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Footer from '../Shared/Footer';
-import Header from '../Shared/Header';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../Shared/Footer";
+import Header from "../Shared/Header";
+import HashLoader from "react-spinners/HashLoader";
 
 const Main = () => {
-    return (
-        <div>
-          <Header/>
-          <Outlet/>
-          <Footer/>
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+  return (
+    <section>
+      {loading ? (
+        <div className="h-screen w-full flex justify-center items-center">
+          <HashLoader
+            color={"#F87272"}
+            loading={loading}
+            size={50}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
         </div>
-    );
+      ) : (
+        <div>
+          <Header />
+          <Outlet />
+          <Footer />
+        </div>
+      )}
+    </section>
+  );
 };
 
 export default Main;
